d16cbb7d3096db19c040c13d4e35a6c8d71c5963
Mohamed Nazeer <st10293093@vcconnect.edu.za>
2025-12-05 23:52:52 +0200

    feat: Implement initial backend setup including Mongoose models, Express server, and database connection.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -23,4 +24,5 @@ app.use("/api/stats", statsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
