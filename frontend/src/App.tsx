e20b69a3c20551e68af2e59d81f7e4587b7faecf
Mohamed Nazeer <st10293093@vcconnect.edu.za>
2025-12-05 22:57:38 +0200

    Initial Project structure and setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+} from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </>
+  );
+}
+
+export default App;
