d4c5639fd59ab68b5416e72b7d9983ee4da29ca2
Mohamed Nazeer <st10293093@vcconnect.edu.za>
2025-12-06 00:07:17 +0200

    feat: Implement initial backend server setup with Express, database connection, and authentication callback for user registration.

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()); // to parse req.body
+
 // When visiting /api/*, use userRoutes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
